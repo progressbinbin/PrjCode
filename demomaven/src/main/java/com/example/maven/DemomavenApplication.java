0b60c8ed1af36050b887f514f2a32012d835be10
binbinyouli <binbin@163.com>
2018-11-20 21:19:40 +0800

    Initial commit

---
+++ demomaven/src/main/java/com/example/maven/DemomavenApplication.java
@@ -0,0 +1,12 @@
+package com.example.maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemomavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemomavenApplication.class, args);
+	}
+}
---
+++ demomaven/src/test/java/com/example/maven/DemomavenApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.maven;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemomavenApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
